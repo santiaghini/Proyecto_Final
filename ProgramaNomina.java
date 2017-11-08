6d09047ee2223b9068fd183f891efe11298f4d6c
santiaghini <santi.hergut@gmail.com>
2017-11-08 09:23:26 -0600

    HOla

--- ProgramaNomina.java
+++ ProgramaNomina.java
@@ -1,28 +1,46 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.Collection;
+import java.util.ArrayList;
 
 public class ProgramaNomina{
+  Scanner lector;
+  List<Map<String>> empleados = new ArrayList<Map<String>>();
+
+
+
   public static void main(String[] args) {
-    Scanner lector = new Scanner (System.in);
-
+    lector = new Scanner (System.in);
     int op;
-    System.out.println("¿Que quieres hacer?");
-    System.out.println("1: Consulta de empleados");
-    System.out.println("2: Agregar empleado");
-    System.out.println("3: Modificar empleado");
-    System.out.println("4: Eliminar empleado");
-    System.out.println("5: Asignaciones y deducciones de empleado");
-    System.out.println("6: Generar nómina");
-    System.out.println("7: Generar recibos");
+    String answer = "s"
 
-    op = lector.nextInt();
-    while (op != 1||op != 2||op != 3||op != 4||op != 5||op != 8||op != 7) {
-      System.out.println("Esa opción no es válida");
+    // Este while permite que puedas repetir diferentes consultas hasta que el usuario decida terminar el programa
+    while (answer.equals("s")){
+
+      System.out.println("¿Qué quieres hacer?");
+      System.out.println("1: Consulta de empleados");
+      System.out.println("2: Agregar empleado");
+      System.out.println("3: Modificar empleado");
+      System.out.println("4: Eliminar empleado");
+      System.out.println("5: Asignaciones y deducciones de empleado");
+      System.out.println("6: Generar nómina");
+      System.out.println("7: Generar recibos");
+
       op = lector.nextInt();
-    }
+      while (op != 1||op != 2||op != 3||op != 4||op != 5||op != 8||op != 7) {
+        System.out.println("Esa opción no es válida, por favor escribe un número en el rango.");
+        op = lector.nextInt();
+      }
 
-    switch (op) {
-      //case 1: consutla();
-      case 2: agregar();
+      switch (op) {
+        //case 1: consulta();
+        case 2: agregar();
+      }
+
+      System.out.println("¿Quieres realizar otra consulta?");
+      System.out.println("s/n");
+      answer = lector.nextLine();
+
     }
 
   }
@@ -36,7 +54,6 @@ public class ProgramaNomina{
   }
 */
   public static void agregar(){
-    Scanner lector = new Scanner (System.in);
 
     int numeroempleados;
     if (numeroempleados == null)
@@ -44,10 +61,6 @@ public class ProgramaNomina{
     else
       numeroempleados++;
 
-
-    String[] nombreempleados = new String [numeroempleados];
-    String[] apellido1empleados = new String [numeroempleados];
-    String[] apellido2empleados = new String [numeroempleados];
 
 
     for (int i=0;i<numeroempleados ;i++ ) {
